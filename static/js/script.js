193fee8b895e05bb4b76a10c079865f86f0bd1a5
wluka85 <wluka85@gmail.com>
2018-09-11 15:05:32 +0200

    Add EventListener to divs

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,32 @@
 import Board from './Board.js';
 
-class Main {
-    static init() {
-        let board = new Board(9);
-        board.drawBoard();
-    }
+var board = new Board(9);
+var sign = "O";
+board.drawBoard();
+addEventListenerToBoard();
+
+
+function addEventListenerToBoard() {
+    let container = document.querySelectorAll('.field');
+    let squareArray = board.squareList;
+
+    container.forEach(function(div, index) {
+        div.addEventListener("click", function() {
+            handleMove(div);
+            sendBoardJSON();
+        });
+    })
 }
 
-Main.init();
+
+function handleMove(div) {
+    let positionStr = div.getAttribute('data-position');
+    let positionArray = positionStr.split('.');
+    div.innerHTML = sign;
+    board.setSign(positionArray[0] - 1, positionArray[1] - 1, sign);
+}
+
+
+function sendBoardJSON(squareList) {
+    console.log(board.squareList);
+}
