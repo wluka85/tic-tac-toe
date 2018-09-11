45d73bb6704066d09e8f5bfb34172381f3442080
wluka85 <wluka85@gmail.com>
2018-09-11 10:42:57 +0200

    Add path to static files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/tic-tac-toe', (req, res) => res.sendFile('index.html', { root: '.' }));
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
