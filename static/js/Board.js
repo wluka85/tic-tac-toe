12df39e4f0112de87f1b6ef65c614e0f1592b555
Cyan <dawid.grygier5@gmail.com>
2018-09-12 11:58:52 +0200

    Added functions to check if someone won the game

--- static/js/Board.js
+++ static/js/Board.js
@@ -3,6 +3,7 @@ import Square from "/js/Square.js";
 export default class Board {
 
     constructor(quantity) {
+        this.win = false;
         this.quantity = quantity;
         this.squareList = this.prepareBoard();
         console.log(this.squareList);
@@ -30,8 +31,6 @@ export default class Board {
         }
     }
 
-
-
     drawBoard() {
         let container = document.getElementById('board');
         let squareArray = this.squareList;
@@ -50,7 +49,7 @@ export default class Board {
     }
 
     isValidMove(x, y) {
-        var flag = false;
+        let flag = false;
         this.squareList.forEach(function(square, index) {
             if(square.x == x && square.y == y) {
                 flag = square.sign.length == 0;
@@ -59,4 +58,48 @@ export default class Board {
 
         return flag;
     }
-}
+
+    isWin(sign) {
+        signCounter(x);
+        signCounter(y);
+        signCounterAcross();
+    }
+
+    signCounterHorizontalVertical(XorYproperty) {
+        let counter1 = 0;
+        let counter2 = 0;
+        let counter3 = 0;
+        this.squareList.forEach(function(square, index) {
+            if(square.XorYproperty == 0 && square.sign == sign){
+                counter1++;
+            }
+            if(square.XorYproperty == 1 && square.sign == sign){
+                counter2++;
+            }
+            if(square.XorYproperty == 2 && square.sign == sign){
+                counter3++;
+            }
+        });
+        if(counter1 == 3 || counter2 == 3 || counter3 == 3){
+            this.win = true;
+            console.log("wygrales gosciu!");
+        }
+    }
+
+    signCounterAcross() {
+        let counter1 = 0;
+        let counter2 = 0;
+        this.squareList.forEach(function(square, index) {
+            if(square.x == square.y && square.sign == sign){
+                counter1++;
+            }
+            if(square.x + square.y == 2 && square.sign == sign){
+                counter2++;
+            }
+        });
+        if(counter1 == 3 || counter2 == 3 || counter3 == 3){
+            this.win = true;
+            console.log("wygrales gosciu!");
+        }
+    }
+}       
