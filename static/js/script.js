e24d5e087cd939de79c6148490c07eb1bcf46746
wluka85 <wluka85@gmail.com>
2018-09-12 10:09:26 +0200

    Implement method sendBoardJSON and refreshBoard and add Button refresh

--- static/js/script.js
+++ static/js/script.js
@@ -3,17 +3,21 @@ import Board from './Board.js';
 var board = new Board(9);
 var sign = "O";
 board.drawBoard();
+var button = document.getElementById("refresh");
+        button.addEventListener("click", function() {
+            refreshBoard(board);
+        });
 addEventListenerToBoard();
 
 
 function addEventListenerToBoard() {
     let container = document.querySelectorAll('.field');
-    let squareArray = board.squareList;
+    let squareList = board.squareList;
 
     container.forEach(function(div, index) {
         div.addEventListener("click", function() {
             handleMove(div);
-            sendBoardJSON();
+            sendBoardJSON(squareList);
         });
     })
 }
@@ -32,5 +36,31 @@ function handleMove(div) {
 
 
 function sendBoardJSON(squareList) {
-    console.log(board.squareList);
+    fetch('/api/board', {
+        method: 'POST',
+        headers: new Headers({'content-type': 'application/json'}),
+        body: JSON.stringify(squareList)
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+        })
 }
+
+function refreshBoard(board) {
+        let container = document.querySelectorAll('.field');
+
+        fetch('/api/board', {
+            method: 'GET'
+        })  // set the path; the method is GET by default, but can be modified with a second parameter
+            .then((response) => response.json())  // parse JSON format into JS object
+            .then((data) => {
+                console.log("tojeto");
+                console.log(data);
+                for (let i=0; i<board.squareList.length; i++) {
+                    container[i].innerHTML = data[i].sign;
+                }
+            })
+
+
+    }
