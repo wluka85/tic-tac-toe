728534b556794d6daf73a7a4608a67c71453f80a
wluka85 <wluka85@gmail.com>
2018-10-04 12:17:55 +0200

    Move view functions to new class BoardView

--- static/js/Square.js
+++ static/js/Square.js
@@ -6,19 +6,6 @@ export default class Square {
         this.sign = '';
     }
 
-    getElement() {
-        let div = document.createElement('div');
-        div.setAttribute('class', 'field');
-        div.setAttribute('data-position', (this.x+1) + '.' + (this.y+1));
-        this.setFillout(div);
-
-        return div;
-    }
-
-    setFillout(div) {
-        div.innerHTML = this.sign;
-    }
-
     setSign(sign) {
         this.sign = sign;
     }
--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,11 @@
 import Board from './Board.js';
+import BoardView from './BoardView.js';
 
 var board = new Board(9);
+var boardView = new BoardView();
 var sign = "";
 
-board.drawBoard();
+boardView.drawBoard(board.squareList);
 
 // var button = document.getElementById("refresh");
 // button.addEventListener("click", function() {
@@ -83,16 +85,11 @@ function refreshBoard(board) {
         })
             .then((response) => response.json())
             .then((data) => {
-                console.log("odświeżam");
-                for (let i=0; i<board.squareList.length; i++) {
-                    container[i].innerHTML = data[i].sign;
-                    if (board.squareList[i].getSign() != data[i].sign) {
-                        board.squareList[i].setSign(data[i].sign);
-                        document.getElementById("board").setAttribute('class', '');
-                    }
-
+                console.log("odświeżam\n" + JSON.stringify(data));
+                for(let i = 0; i < board.squareList.length; i++) {
+                    board.squareList[i].setSign(data[i].sign);
                 }
-
+                boardView.refreshBoard(board.squareList);
             })
 }
 
